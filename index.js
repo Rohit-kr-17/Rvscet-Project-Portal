d4c8a4e0ab2cad55476257931d923ff0fbf23487
Rohit-kr-17 <rohit.2001.chaurasia@gmail.com>
2022-07-03 12:53:07 +0530

    commit

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const passport = require("passport");
 const passportLocal = require("./config/passport-local-strategy");
 const MongoStore = require("connect-mongo")(session);
 
+const flash = require("connect-flash");
+const customMware = require("./config/middleware");
+
 app.use(express.urlencoded());
 
 app.use(cookieParser());
@@ -28,7 +31,7 @@ app.set("views", "./views");
 // mongo store is used to store the session cookie in the db
 app.use(
 	session({
-		name: "Project Management",
+		name: "codeial",
 		// TODO change the secret before deployment in production mode
 		secret: "blahsomething",
 		saveUninitialized: false,
@@ -52,6 +55,9 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(passport.setAuthenticatedUser);
+
+app.use(flash());
+app.use(customMware.setFlash);
 
 // use express router
 app.use("/", require("./routes"));
--- models/post.js
+++ models/post.js
@@ -1,26 +1,35 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
+const postSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		software: {
+			type: String,
+			required: true,
+		},
+		content: {
+			type: String,
+			required: true,
+		},
+		user: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+		},
+		// include the array of ids of all comments in this post schema itself
+		comments: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "Comment",
+			},
+		],
+	},
+	{
+		timestamps: true,
+	}
+);
 
-const postSchema = new mongoose.Schema({
-    content: {
-        type: String,
-        required: true
-    },
-    user: {
-        type:  mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-
-    },
-    // include the array of ids of all comments in this post schema itself
-    comments: [
-        {
-            type:  mongoose.Schema.Types.ObjectId,
-            ref: 'Comment'
-        }
-    ]
-},{
-    timestamps: true
-});
-
-const Post = mongoose.model('Post', postSchema);
+const Post = mongoose.model("Post", postSchema);
 module.exports = Post;
